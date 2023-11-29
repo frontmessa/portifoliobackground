d23ebb59a9f9df1eb1652e8292258cac375f6a8f
frontmessa <caio-messa@hotmail.com>
2023-11-29 14:59:50 -0300

    remove nav

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,15 @@
 
 //
-$(document).ready(function() {
-			$('#fullpage').fullpage({
-				'verticalCentered': false,
-				'scrollingSpeed': 600,
-				'autoScrolling': false,
-				'css3': true,
-				'navigation': true,
-				'navigationPosition': 'right',
-			});
-		});
+// $(document).ready(function() {
+// 			$('#fullpage').fullpage({
+// 				'verticalCentered': false,
+// 				'scrollingSpeed': 600,
+// 				'autoScrolling': false,
+// 				'css3': true,
+// 				'navigation': false,
+// 				'navigationPosition': 'right',
+// 			});
+// 		});
 
 // wow
 $(function()
